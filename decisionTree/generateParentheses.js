68946a5841c9d40f3bdd68e729a46336fd147aff
Jae Shin <jae5464@gmail.com>
2016-09-08 12:18:55 -0700

    Move generateParentheses into decisionTree folder

--- decisionTree/generateParentheses.js
+++ decisionTree/generateParentheses.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Category: Recursion, Decision tree
  * 
  * Given n pairs of parentheses, write a function to generate all combinations
@@ -9,35 +9,35 @@
  */
 var generateParentheses = function(n) {
   const result = [];
-  const innerRecursiveFunc = function(left, right, str) {
+  const buildStr = function(left, right, str) {
     if (left === n) {
       if (right === n) {
         result.push(str);
         return;
       } else if (right < n) {
-        return innerRecursiveFunc(left, right + 1, str + ')');
+        buildStr(left, right + 1, str + ')');
       }
     } else if (left < n) {
       if (left > right) { 
-        innerRecursiveFunc(left + 1, right, str + '(');
-        innerRecursiveFunc(left, right + 1, str + ')');
+        buildStr(left + 1, right, str + '(');
+        buildStr(left, right + 1, str + ')');
       } else {
-        innerRecursiveFunc(left + 1, right, str + '(');
+        buildStr(left + 1, right, str + '(');
       }
     }
   };
-  innerRecursiveFunc(0, 0, '');
+  buildStr(0, 0, '');
   return result;
 };
 
 
 // Testing
-console.log(generateParentheses(0));
-console.log(generateParentheses(1));
-console.log(generateParentheses(2));
+// console.log(generateParentheses(0));
+// console.log(generateParentheses(1));
+// console.log(generateParentheses(2));
 console.log(generateParentheses(3));
-console.log(generateParentheses(4));
-console.log(generateParentheses(5));
+// console.log(generateParentheses(4));
+// console.log(generateParentheses(5));
 /*
   [
     "((()))",
