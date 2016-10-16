1638d78a1f46f5e6eccb164b06f19d51d5605e11
Jae Shin <jae5464@gmail.com>
2016-10-16 15:49:38 -0700

    Implement isBalanced and listOfDepths for binary tree

--- dataStructures/binaryTree.js
+++ dataStructures/binaryTree.js
@@ -15,7 +15,7 @@ class BinaryTree {
     return this;
   }
 
-  changeValTo(value) {
+  changeValueTo(value) {
     this.value = value;
   }
 
@@ -84,10 +84,81 @@ binaryTree.left.addLeft(4).addRight(5);
 binaryTree.right.addLeft(6).addRight(7);
 // console.log(binaryTree);
 // binaryTree.inOrderTraversal(node => console.log(node.value));
-
 // inOrderTraversal(binaryTree, node => console.log(node.value));
 // preOrderTraversal(binaryTree, node => console.log(node.value));
 // postOrderTraversal(binaryTree, node => console.log(node.value));
-breadthFirstSearch(binaryTree, node => console.log(node.value));
+// breadthFirstSearch(binaryTree, node => console.log(node.value));
+
+/*
+       1
+    2     3
+  4  5
+ */
+
+const binaryTree2 = new BinaryTree(1);
+binaryTree2.addLeft(2).addRight(3);
+binaryTree2.left.addLeft(4).addRight(5);
 
 
+// Check Balanced
+const isBalanced = function(root) {
+  const balancedWithDepth = function(node) {
+    if (node === null) {
+      return {balanced: true, depth: -1};
+    }
+
+    let left = balancedWithDepth(node.left);
+    if (left.balanced) {
+      let right = balancedWithDepth(node.right);
+      if (right.balanced && Math.abs(left.depth - right.depth) <= 1) {
+        return {balanced: true, depth: Math.max(left.depth, right.depth) + 1};
+      }
+    }
+
+    return {balanced: false, depth: null};
+  }
+
+  return balancedWithDepth(root).balanced;
+};
+
+console.log(isBalanced(null), true);
+console.log(isBalanced(new BinaryTree(1)), true);
+console.log(isBalanced(new BinaryTree(1).addLeft(2)), true);
+
+let btree = new BinaryTree(1);
+btree.addLeft(2);
+btree.left.addRight(3);
+console.log(isBalanced(btree), false);
+console.log(isBalanced(binaryTree), true);
+console.log(isBalanced(binaryTree2), true);
+
+
+
+// Given a binary tree, design an algo which creates a linked list of all the nodes at each depth
+const listOfDepths = function(root) {
+  // init array to hold the linkedlists
+  const array = [];
+  let llist;
+
+  // perform a pre-order DFS traversal
+
+  const dfs = function(node, depth) {
+    if (node === null) {
+      return;
+    }
+
+    if (array[depth] === undefined) {
+      llist = new LinkedList();
+      array[depth] = llist;
+    } else {
+      llist = array[depth];
+    }
+
+    llist.addToTail(node);
+
+    dfs(node.left, depth + 1);
+    dfs(node.right, depth + 1);
+  };
+
+  return array;
+};
